0c9c9baa28e8c9934119a3cff55da7497110ef76
sommano <sommano@gmail.com>
2018-06-14 14:18:01 -0400

    cha24

---
+++ dummies/C24/L2406.c
@@ -0,0 +1,25 @@
+#include "ex2403.h"
+
+human person;
+
+int main()
+{
+    fillstructure();
+    printstructure();
+    return 0;
+}
+
+void fillstructure(void)
+{
+    printf("Enter your name: ");
+    fgets(person.name,31,stdin);
+    printf("Enter your age: ");
+    scanf("%d",&person.age);
+
+}
+
+void printstructure(void)
+{
+    printf("You are %s\n",person.name);
+    printf("And you are %d years old.\n",person.age);
+}
