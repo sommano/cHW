0674b9ee0a51d89a203847dbd326e63363924784
sommano <sommano@gmail.com>
2018-06-20 15:25:59 -0400

    ch9

---
+++ T21c/ch09/getstrings.c
@@ -0,0 +1,14 @@
+#define _GNU_SOURCE //cause stdio.h to include vasprintf
+#include <stdio.h>
+#include <stdlib.h> //free
+
+void get_strings(char const *in){
+    char *cmd;
+    asprintf(&cmd, "strings %s", in);
+    if (system(cmd)) fprintf(stderr, "something went wrong running %s.\n", cmd);
+    free(cmd);
+    }
+
+int main(int argc, char **argv){
+    get_strings(argv[0]);
+}
