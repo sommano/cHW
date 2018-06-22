327868f025e1753f0efc2c9433ac3e0612f3cfab
sommano <sommano@gmail.com>
2018-06-22 15:04:01 -0400

    ch10

---
+++ T21c/ch10/sum_to_nan.c
@@ -0,0 +1,15 @@
+#include <math.h> //NAN
+#include <stdio.h>
+
+double sum(double in[]){
+    double out=0;
+    for (int i=0; !isnan(in[i]); i++) out += in[i];
+    return out;
+}
+
+int main(){
+    double list[] = {1.1, 2.2, 3.3, NAN};
+    printf("sum: %g\n", sum(list));
+    
+    printf("sum: %g\n", sum((double[]){1.1, 2.2, 3.3, NAN}));
+}
