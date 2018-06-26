db04a861acd0dc188a8c322ef9f8777410531bf5
sommano <sommano@gmail.com>
2018-06-26 15:12:43 -0400

    cha10

---
+++ T21c/ch10/macro_varargs.c
@@ -0,0 +1,18 @@
+#define _GNU_SOURCE //cause stdio.h to include vasprintf
+#include <stdio.h> //printf, vasprintf
+#include <stdlib.h> //system
+#include <assert.h>
+
+#define System_w_printf(outval, ...) {          \
+    char *string_for_systemf;                   \
+    asprintf(&string_for_systemf, __VA_ARGS__); \
+    outval = system(string_for_systemf);        \
+    free(string_for_systemf);                   \
+}
+
+int main(int argc, char **argv){
+    assert(argc == 2);
+    int out;
+    System_w_printf(out, "ls %s", argv[1]);
+    return out;
+}
--- T21c/ch10/olden_varargs.c
+++ T21c/ch10/olden_varargs.c
@@ -5,18 +5,19 @@
 #include <assert.h>
 
 int system_w_printf(char const *fmt, ...) \
-__attribute__ ((format (printf,1,2)));
+        __attribute__ ((format (printf,1,2)));
+
 int system_w_printf(char const *fmt, ...){
-char *cmd;
-va_list argp;
-va_start(argp, fmt);
-vasprintf(&cmd, fmt, argp);
-va_end(argp);
-int out= system(cmd);
-free(cmd);
-return out;
+    char *cmd;
+    va_list argp;
+    va_start(argp, fmt);
+    vasprintf(&cmd, fmt, argp);
+    va_end(argp);
+    int out= system(cmd);
+    free(cmd);
+    return out;
 }
 int main(int argc, char **argv){
-assert(argc == 2);
-return system_w_printf("ls %s", argv[1]);
+    assert(argc == 2);
+    return system_w_printf("ls %s", argv[1]);
 }
