1da15666839ea8ea64ecbb58dd6eb6cba731eefe
sommano <sommano@gmail.com>
2018-07-05 15:23:02 -0400

    ch11

---
+++ T21c/ch11/dict_use.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dict.h"
+
+int main(){
+    int zero = 0;
+    float one = 1.0;
+    char two[] = "two";
+    dictionary *d = dictionary_new();
+    dictionary_add(d, "an int", &zero);
+    dictionary_add(d, "a float", &one);
+    dictionary_add(d, "a string", &two);
+    printf("The integer I recorded was: %i\n", *(int*)dictionary_find(d, "an int"));
+    printf("The string was: %s\n", (char*)dictionary_find(d, "a string"));
+    dictionary_free(d);
+}
+
