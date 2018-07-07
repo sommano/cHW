b566d8645315ccffe86d85bd0d476cece0c2c339
sommano <sommano@gmail.com>
2018-07-07 14:21:55 -0400

    ch11

---
+++ T21c/ch11/groups.h
@@ -0,0 +1,13 @@
+#include <apop.h>
+#include <glib.h>
+
+typedef struct {
+    gsl_vector *position;
+    int id, size;
+} group_s;
+
+group_s* group_new(gsl_vector *position);
+group_s* group_join(group_s *joinme, gsl_vector *position);
+void group_exit(group_s *leaveme, gsl_vector *position);
+group_s* group_closest(gsl_vector *position, double mb);
+void print_groups();
