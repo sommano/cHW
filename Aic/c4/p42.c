8181da01bff3302299240e03c9ac458b6892dec5
sommano <sommano@gmail.com>
2018-09-16 17:58:20 -0400

    ch4

--- Aic/c4/p42.c
+++ Aic/c4/p42.c
@@ -5,5 +5,17 @@
 
 main(int argc, char *argv[])
 {
-    STACKinit(N)
+    STACKinit(N);
+    for (i = 0; i < N; i++)
+    {
+        if (a[i] == '+')
+            STACKpush(STACKpop()+STACKpop());
+        if (a[i] == '*')
+            STACKpush(STACKpop()+STACKpop());
+        if ((a[i] >= '0') && (a[i] <= '9'))
+            STACKpush(0);
+        while ((a[i] >= '0') && (a[i++]-'0'));
+            STACKpush(10*STACKpop() + (a[i++]-'0'));
+    }
+    printf("%d \n", STACLpop());
 }
